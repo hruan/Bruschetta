d15423f4a58d0dd9a593adebbdf30bc86637a88a
Hang Ruan <h@ruan.se>
2013-03-08 09:34:34 +0100

    Use strict

--- bin/content/js/main.js
+++ bin/content/js/main.js
@@ -1,37 +1,36 @@
 (function AppData() {
-    var self = this;
-
-    self.searchTerm = ko.observable();
-
-    self.titles = ko.mapping.fromJS([]);
-
-    self.search = function() {
-        if (searchTerm === undefined || searchTerm === '') return;
-        $.ajax({
-            url: 'api/1/search?q=' + self.searchTerm(),
-            success: searchCallback,
-            error: function(request, textStatus, errorThrown) {
-                alert('search failed:', textStatus);
-            }
-        });
+    'use strict';
+    var viewModel = {
+        titles: ko.mapping.fromJS([]),
+        searchTerm: ko.observable(),
+        search: function() {
+            var searchTerm = this.searchTerm();
+            if (searchTerm === undefined || searchTerm === '') return;
+            $.ajax({
+                url: 'api/1/search?q=' + searchTerm,
+                context: this,
+                success: this.searchCallback,
+                error: function(request, textStatus, errorThrown) {
+                    alert('search failed:', textStatus);
+                }
+            });
+        },
+        searchCallback: function(result) {
+            ko.mapping.fromJS(result, this.titles);
+            result.forEach(this.getReviews);
+        },
+        getReviews: function(elem) {
+            var url = 'api/1/reviews/' + elem.id;
+            $.getJSON(url, function(data) {
+                $("#" + elem.id).html("Critics score: " + data.ratings.critics_score);
+            })
+            .error(function() {
+                $("#" + elem.id).html("No reviews!");
+            });
+        }
     };
 
-    self.searchCallback = function(result) {
-        ko.mapping.fromJS(result, self.titles);
-        result.forEach(getReviews);
-    };
-
-    self.getReviews = function(elem) {
-        var url = 'api/1/reviews/' + elem.id;
-        $.getJSON(url, function(data) {
-            $("#" + elem.id).html("Critics score: " + data.ratings.critics_score);
-        })
-        .error(function() {
-            $("#" + elem.id).html("No reviews!");
-        });
-    };
-
-    ko.applyBindings(this);
+    ko.applyBindings(viewModel);
 })();
 
 /*(function () {
